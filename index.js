2b29240c246632f223b1ce8250cf8213221ae5ab
duonguyen321 <codebyduong@gmail.com>
2024-11-22 16:47:08 +0700

    push code

--- index.js
+++ index.js
@@ -31,71 +31,75 @@ const wafSession = require('./endpoints/wafSession')
 const chalk = require("chalk");
 
 app.post(pathParam, async (req, res) => {
-    const data = req.body
-    console.log('data: ', data)
+    try {
+        const data = req.body
+        console.log('data: ', data)
 
-    const check = reqValidate(data)
+        const check = reqValidate(data)
 
-    if (check !== true) return res.status(400).json({code: 400, message: 'Bad Request', schema: check})
+        if (check !== true) return res.status(400).json({code: 400, message: 'Bad Request', schema: check})
 
-    if (authToken && data.authToken !== authToken) return res.status(401).json({code: 401, message: 'Unauthorized'})
+        if (authToken && data.authToken !== authToken) return res.status(401).json({code: 401, message: 'Unauthorized'})
 
-    if (global.browserLength >= global.browserLimit) return res.status(429).json({
-        code: 429,
-        message: 'Too Many Requests'
-    })
+        if (global.browserLength >= global.browserLimit) return res.status(429).json({
+            code: 429,
+            message: 'Too Many Requests'
+        })
 
-    if (process.env.SKIP_LAUNCH !== 'true' && !global.browser) return res.status(500).json({
-        code: 500,
-        message: 'The scanner is not ready yet. Please try again a little later.'
-    })
+        if (process.env.SKIP_LAUNCH !== 'true' && !global.browser) return res.status(500).json({
+            code: 500,
+            message: 'The scanner is not ready yet. Please try again a little later.'
+        })
 
-    let result = {code: 500};
+        let result = {code: 500};
 
-    global.browserLength++
+        global.browserLength++
 
-    switch (data.mode) {
-        case "source": {
-            console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
-            result = await getSource(data).then(res => {
-                return {source: res, code: 200}
-            }).catch(err => {
-                return {code: 500, message: err.message}
-            })
-            break;
+        switch (data.mode) {
+            case "source": {
+                console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
+                result = await getSource(data).then(res => {
+                    return {source: res, code: 200}
+                }).catch(err => {
+                    return {code: 500, message: err.message}
+                })
+                break;
+            }
+            case "turnstile-min": {
+                console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
+                result = await solveTurnstileMin(data).then(res => {
+                    return {token: res, code: 200}
+                }).catch(err => {
+                    return {code: 500, message: err.message}
+                })
+                break;
+            }
+            case "turnstile-max": {
+                console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
+                result = await solveTurnstileMax(data).then(res => {
+                    return {token: res, code: 200}
+                }).catch(err => {
+                    return {code: 500, message: err.message}
+                })
+                break;
+            }
+            case "waf-session": {
+                console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
+                result = await wafSession(data).then(res => {
+                    return {...res, code: 200}
+                }).catch(err => {
+                    return {code: 500, message: err.message}
+                })
+                break;
+            }
         }
-        case "turnstile-min": {
-            console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
-            result = await solveTurnstileMin(data).then(res => {
-                return {token: res, code: 200}
-            }).catch(err => {
-                return {code: 500, message: err.message}
-            })
-            break;
-        }
-        case "turnstile-max": {
-            console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
-            result = await solveTurnstileMax(data).then(res => {
-                return {token: res, code: 200}
-            }).catch(err => {
-                return {code: 500, message: err.message}
-            })
-            break;
-        }
-        case "waf-session": {
-            console.info(chalk.green(`Call ${data.url} by mode ${data.mode}`))
-            result = await wafSession(data).then(res => {
-                return {...res, code: 200}
-            }).catch(err => {
-                return {code: 500, message: err.message}
-            })
-            break;
-        }
+
+        global.browserLength--
+
+        res.status(result.code ?? 500).send(result)
+    } catch (e) {
+        console.error(chalk.red(e.message))
     }
-
-    global.browserLength--
-
-    res.status(result.code ?? 500).send(result)
 })
 
 app.use((req, res) => {
